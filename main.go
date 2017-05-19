1817952a932a9097e1bf6f548f4c0324a5ea2d8d
Wian Vos <wian.vos@cinqict.nl>
2017-05-19 07:59:33 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+)
+
+var routes = Routes{
+	Route{"List", "GET", "/Transactions", List},
+}
+
+var transactions = Transactions{
+	Transaction{"Debit", "$50", "amazon.com", time.Date(2017, 11, 17, 20, 34, 58, 651387237, time.UTC)},
+	Transaction{"Debit", "$154", "ebay.com", time.Date(2017, 11, 17, 20, 34, 58, 651387237, time.UTC)},
+	Transaction{"Debit", "$15", "netflix.com", time.Date(2017, 11, 18, 20, 34, 58, 651387237, time.UTC)},
+	Transaction{"Debit", "$40", "etsy.com", time.Date(2017, 11, 18, 11, 34, 58, 651387237, time.UTC)},
+	Transaction{"Debit", "$500", "homedepot", time.Date(2017, 11, 18, 20, 34, 58, 651387237, time.UTC)},
+	Transaction{"Debit", "$50", "wholefoods", time.Date(2017, 11, 18, 20, 34, 58, 651387237, time.UTC)},
+	Transaction{"Debit", "$70", "slagerij van kampen", time.Date(2017, 11, 19, 20, 34, 58, 651387237, time.UTC)},
+	Transaction{"Debit", "$55", "conrad.nl", time.Date(2017, 11, 19, 20, 34, 58, 651387237, time.UTC)},
+	Transaction{"Credit", "$4200", "Carebear Inc.", time.Date(2017, 11, 20, 20, 34, 58, 651387237, time.UTC)},
+}
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+type Transaction struct {
+	Type         string    `json:"Type"`
+	Amount       string    `json:"Amount"`
+	CounterParty string    `json:"CounterParty"`
+	Date         time.Time `json:"Date"`
+}
+
+type Transactions []Transaction
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	return router
+}
+
+func main() {
+	router := NewRouter()
+	router.HandleFunc("/", logHandler(MessageHandler))
+	spew.Dump(router)
+	log.Fatal(http.ListenAndServe(":3000", router))
+
+}
+
+func List(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	if err := json.NewEncoder(w).Encode(transactions); err != nil {
+		panic(err)
+	}
+}
+
+func logHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		x, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		log.Println(fmt.Sprintf("%q", x))
+		rec := httptest.NewRecorder()
+		fn(rec, r)
+		log.Println(fmt.Sprintf("%q", rec.Body))
+	}
+}
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "A message was received")
+}
